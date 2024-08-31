64226555eacebb71c0a2c45e200fcbefa5a6cf20
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-08-31 08:08:56 -0400

    display table of books read

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,48 @@ import React from "react";
 import Header from "../components/Header";
 import "../css/Home.css";
 import { Link } from "react-router-dom";
+import { bookData } from "../types/bookData";
 
 const Home = () => {
+    const bookInfo : bookData[] = JSON.parse(localStorage.getItem("books") ?? "[]");
+
     return (
         <>
             <Header />
             <main className="home-body">
-                <div className={`w-[4%] min-w-max rounded-lg bg-blue-700 p-8 flex justify-center items-center
-                    text-center text-5xl mx-auto`}>
+                <div className={`w-[4%] min-w-max rounded-lg bg-slate-800 p-8 flex justify-center items-center
+                    text-center text-5xl mx-auto border-2 border-white`}>
                     <Link to="/add">Add Book</Link>
+                </div>
+                <div id="info" className="w-full my-8">
+                    <table className="w-2/3 min-w-max mx-auto ">
+                        <thead>
+                            <tr>
+                                <th>Book ID</th>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Pages Read</th>
+                                <th>Total Pages</th>
+                                <th>% Complete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                bookInfo.map((book: bookData, index: number) => {
+                                    return (
+                                        <tr data-highlight={String(book.pagesRead === book.pages)}>
+                                            <td>{index + 1}</td>
+                                            <td>{book.title}</td>
+                                            <td>{book.author ?? "--"}</td>
+                                            <td>{book.pagesRead}</td>
+                                            <td>{book.pages}</td>
+                                            <td>{Math.round((book.pagesRead / book.pages) * 1000)/10}%</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </main>
         </>
